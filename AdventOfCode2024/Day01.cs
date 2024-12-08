15ad09a0c787ef4363232918f9ee58c4adfd3c8e
Benna96 <vissersofia@gmail.com>
2024-12-08 20:11:29 +0200

    Add day 1 skeleton
    Should have everything needed, next can start implementing things.

---
+++ AdventOfCode2024/Day01.cs
@@ -0,0 +1,29 @@
+namespace AdventOfCode2024;
+
+/// <summary>
+/// <see href="https://adventofcode.com/2024/day/1">Link to puzzle</see> 
+/// </summary>
+public sealed class Day01 : BaseDay
+{
+    private readonly string _inputDummy;
+    
+    public Day01()
+    {
+        _inputDummy = ParseInput(File.ReadAllText(InputFilePath));
+    }
+    
+    public static string ParseInput(string input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
+}
