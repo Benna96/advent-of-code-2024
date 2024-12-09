7c0f8fea24c0bbcd3e5dda1f74a2f72cf0f5f508
Benna96 <vissersofia@gmail.com>
2024-12-09 02:54:32 +0200

    Change variable names in day 1 tests
    Makes the intent clearer.
    Day -> GivenDay
    DemoInput -> GivenInput
    DemoLeftIds -> ExpectedLeftIds
    DemoRightIds -> ExpectedRightIds

--- AdventOfCode2024.Tests/Day01Tests.cs
+++ AdventOfCode2024.Tests/Day01Tests.cs
@@ -4,9 +4,9 @@ namespace AdventOfCode2024.Tests;
 
 public class Day01Tests
 {
-    private static Day01 Day => new(DemoInput);
+    private static Day01 GivenDay => new(GivenInput);
 
-    private static readonly string DemoInput = """
+    private static readonly string GivenInput = """
                                                3   4
                                                4   3
                                                2   5
@@ -15,32 +15,32 @@ public class Day01Tests
                                                3   3
                                                """;
 
-    private static readonly IReadOnlyList<int> DemoLeftIds = [3, 4, 2, 1, 3, 3];
-    private static readonly IReadOnlyList<int> DemoRightIds = [4, 3, 5, 3, 9, 3];
+    private static readonly IReadOnlyList<int> ExpectedLeftIds = [3, 4, 2, 1, 3, 3];
+    private static readonly IReadOnlyList<int> ExpectedRightIds = [4, 3, 5, 3, 9, 3];
 
     [Fact]
     public void InputIsParsedCorrectly()
     {
-        var parsed = Day01.ParseInput(DemoInput);
+        var parsed = Day01.ParseInput(GivenInput);
 
         using (new AssertionScope())
         {
-            parsed.leftIds.Should().BeEquivalentTo(DemoLeftIds, "values should match input's first column");
-            parsed.rightIds.Should().BeEquivalentTo(DemoRightIds, "values should match input's second column");
+            parsed.leftIds.Should().BeEquivalentTo(ExpectedLeftIds, "values should match input's first column");
+            parsed.rightIds.Should().BeEquivalentTo(ExpectedRightIds, "values should match input's second column");
         }
     }
     
     [Fact]
     public async Task Part1SolverIsWip()
     {
-        var solver1 = async () => await Day.Solve_1();
+        var solver1 = async () => await GivenDay.Solve_1();
         await solver1.Should().ThrowAsync<NotImplementedException>();
     }
 
     [Fact]
     public async Task Part2SolverIsWip()
     {
-        var solver2 = async () => await Day.Solve_2();
+        var solver2 = async () => await GivenDay.Solve_2();
         await solver2.Should().ThrowAsync<NotImplementedException>();
     }
 }
