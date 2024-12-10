29b27af13eaad3a3c93cd3f68da20466fbc7c1e3
Benna96 <vissersofia@gmail.com>
2024-12-10 18:01:32 +0200

    Implement day 3 part 2 input parsing
    Part 1 takes different stuff from input than part 2.
    Maybe input parsing should be part of solver instead...
    
    Test input for part 2 is different, the test is messy rn.

--- AdventOfCode2024.Tests/Day03Tests.cs
+++ AdventOfCode2024.Tests/Day03Tests.cs
@@ -6,16 +6,24 @@ public sealed class Day03Tests : BaseProblemTests<Day03>
 
     private static string GivenInput => "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 
-    private static IReadOnlyList<string> ExpectedValidInstructions =>
+    private static IReadOnlyList<string> ExpectedValidInstructionsPart1 =>
         ["mul(2,4)", "mul(5,5)", "mul(11,8)", "mul(8,5)"];
+
+    private static string GivenInput2 => "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
+    private static IReadOnlyList<string> ExpectedValidInstructionsPart2 =>
+        ["mul(2,4)", "mul(8,5)"];
 
     protected override string ExpectedPart1Solution => "161";
     protected override string ExpectedPart2Solution => "TODO";
 
     public override void InputIsParsedCorrectly()
     {
-        var parsed = Day03.ParseInput(GivenInput);
-        parsed.Should().BeEquivalentTo(ExpectedValidInstructions);
+        var parsed = Day03.ParseInputPart1(GivenInput);
+        parsed.Should().BeEquivalentTo(ExpectedValidInstructionsPart1);
+        
+        var parsed2 = Day03.ParseInputPart2(GivenInput2);
+        parsed2.Should().BeEquivalentTo(ExpectedValidInstructionsPart2);
     }
     
     [Fact(Skip = "TODO")]
--- AdventOfCode2024/Day03.cs
+++ AdventOfCode2024/Day03.cs
@@ -5,21 +5,61 @@ namespace AdventOfCode2024;
 public sealed class Day03 : BaseDay
 {
     private readonly IReadOnlyList<string> _validInstructions;
+    private readonly IReadOnlyList<string> _validInstructionsPart2;
     
     public Day03() : this(null) { }
 
     public Day03(string? inputOverride = null)
     {
         var input = this.FetchInput(inputOverride);
-        _validInstructions = ParseInput(input);
+        _validInstructions = ParseInputPart1(input);
+        _validInstructionsPart2 = ParseInputPart2(input);
     }
     
-    public static IReadOnlyList<string> ParseInput(string input)
+    public static IReadOnlyList<string> ParseInputPart1(string input)
     {
         var regex = new Regex(@"mul\(\d{1,3},\d{1,3}\)");
         var matches = regex.Matches(input);
         var validInstructions = matches.Select(x => x.ToString());
         return validInstructions.ToList().AsReadOnly();
+    }
+
+    public static IReadOnlyList<string> ParseInputPart2(string input)
+    {
+        var mulRegex = @"mul\(\d{1,3},\d{1,3}\)";
+        var doRegex = @"do\(\)";
+        var dontRegex = @"don't\(\)";
+        
+        var regex = new Regex($@"(?<mul>{mulRegex})|(?<do>{doRegex})|(?<dont>{dontRegex})");
+        var matches = regex.Matches(input);
+
+        var valid = true;
+        List<string> validInstructions = [];
+
+        foreach (Match match in matches)
+        {
+            var group = match.Groups.Values.Skip(1).First(x => x.Success)!;
+            
+            switch (group.Name)
+            {
+                case "mul":
+                    if (valid)
+                    {
+                        validInstructions.Add(group.Value);
+                    }
+                    break;
+                
+                case "do":
+                    valid = true;
+                    break;
+                
+                case "dont":
+                    valid = false;
+                    break;
+            }
+        }
+
+        return validInstructions.AsReadOnly();
     }
 
     public override ValueTask<string> Solve_1()
