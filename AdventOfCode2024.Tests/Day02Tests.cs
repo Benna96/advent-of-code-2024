ddb013e8ed8bc5901554a3b1889d383c98c684f0
Benna96 <vissersofia@gmail.com>
2024-12-10 06:04:21 +0200

    Implement day 2 input parsing

--- AdventOfCode2024.Tests/Day02Tests.cs
+++ AdventOfCode2024.Tests/Day02Tests.cs
@@ -4,15 +4,32 @@ public sealed class Day02Tests : BaseProblemTests<Day02>
 {
     protected override Day02 GivenProblem { get; } = new(GivenInput);
 
-    private static string GivenInput => "TODO";
+    private static string GivenInput => """
+                                        7 6 4 2 1
+                                        1 2 7 8 9
+                                        9 7 6 2 1
+                                        1 3 2 4 5
+                                        8 6 4 4 1
+                                        1 3 6 7 9
+                                        """;
+
+    private static IReadOnlyList<IReadOnlyList<int>> ExpectedLevelReports =>
+    [
+        [7, 6, 4, 2, 1],
+        [1, 2, 7, 8, 9],
+        [9, 7, 6, 2, 1],
+        [1, 3, 2, 4, 5],
+        [8, 6, 4, 4, 1],
+        [1, 3, 6, 7, 9],
+    ];
 
     protected override string ExpectedPart1Solution => "TODO";
     protected override string ExpectedPart2Solution => "TODO";
 
-    [Fact(Skip = "TODO")]
     public override void InputIsParsedCorrectly()
     {
-        throw new NotImplementedException();
+        var parsed = Day02.ParseInput(GivenInput);
+        parsed.Should().BeEquivalentTo(ExpectedLevelReports);
     }
 
     [Fact(Skip = "TODO")]
--- AdventOfCode2024/Day02.cs
+++ AdventOfCode2024/Day02.cs
@@ -2,16 +2,28 @@ namespace AdventOfCode2024;
 
 public sealed class Day02 : BaseDay
 {
+    private readonly IReadOnlyList<IReadOnlyList<int>> _levelReports;
+    
     public Day02() : this(null) { }
 
     public Day02(string? inputOverride = null)
     {
         var input = this.FetchInput(inputOverride);
+        _levelReports = ParseInput(input);
     }
-    
-    public static object ParseInput(string input)
+
+    public static IReadOnlyList<IReadOnlyList<int>> ParseInput(string input)
     {
-        throw new NotImplementedException();
+        List<IReadOnlyList<int>> levelReports = [];
+        
+        using var reader = new StringReader(input);
+        while (reader.ReadLine() is { } line)
+        {
+            var levels = line.Split(' ').Select(int.Parse);
+            levelReports.Add(levels.ToList().AsReadOnly());
+        }
+        
+        return levelReports;
     }
 
     public override ValueTask<string> Solve_1()
